f6f63fe68982616ca4def5810ade01644813ea53
lkc <stone111r@gmail.com>
2018-11-07 22:12:20 +0800

    Update

--- components/chung-image/index.tsx
+++ components/chung-image/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react'
 import {Image, ImageProps} from "react-native";
 
-class ChungImage extends React.Component<ImageProps>{
-    render(){
-        let {props} = this;
-        return (
-            <Image {...props}/>
-        )
-    }
-}
+// class ChungImage extends React.Component<ImageProps>{
+//     render(){
+//         let {props} = this;
+//         return (
+//             <Image {...props}/>
+//         )
+//     }
+// }
+
+const ChungImage = (props:ImageProps)=>{
+    return (
+        <Image {...props}/>
+    )
+};
+
+export default ChungImage;
--- components/chung-view/index.tsx
+++ components/chung-view/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
-import {View, ViewProps} from "react-native";
+import {Text, TextProps, View, ViewProps} from "react-native";
 
 
-class ChungView extends React.Component<ViewProps>{
-    render(){
-        let {props} = this;
-        return (
-            <View {...props}/>
-        )
-    }
-}
+// class ChungView extends React.Component<ViewProps>{
+//     render(){
+//         let {props} = this;
+//         return (
+//             <View {...props}/>
+//         )
+//     }
+// }
+
+
+const ChungView = (props:ViewProps)=>{
+    return (
+        <View {...props}/>
+    )
+};
+
+export default ChungView;
