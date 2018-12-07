486134569e908b978032474fe1d5ad690e6a7eec
lkc <stone111r@gmail.com>
2018-12-07 17:18:53 +0800

    Update components

--- components/hint-text/index.tsx
+++ components/hint-text/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {ReactNode} from 'react'
-import {Text} from "react-native";
+import {Text, ViewStyle} from "react-native";
 import Styles from "../style";
 import ChungView from "../chung-view";
 import UIContext from "../ui-provider/UIContext";
@@ -9,22 +9,26 @@ export interface HintTextProps {
     text: string;
     icon?: ReactNode;
     color?: string;
+    containerStyle?: ViewStyle
 }
 
-const HintText = ({color, icon, text}: HintTextProps) => {
+const HintText = ({color, icon, text,containerStyle}: HintTextProps) => {
     return (
         <UIContext.Consumer>
             {
                 ({isDarkMode}) => {
                     color = color || Styles.hintTextDefaultTextColor;
                     return (
-                        <ChungView style={{
-                            borderColor: color,
-                            borderWidth: 1,
-                            padding: Styles.padding,
-                            marginVertical: Styles.margin
-                        }}>
-                            <Text style={[{lineHeight: 24,color}]}>
+                        <ChungView style={[
+                            {
+                                borderColor: color,
+                                borderWidth: 1,
+                                padding: Styles.padding,
+                                marginVertical: Styles.margin
+                            },
+                            containerStyle
+                        ]}>
+                            <Text style={[{lineHeight: 24, color}]}>
                                 {icon} {text}
                             </Text>
                         </ChungView>
