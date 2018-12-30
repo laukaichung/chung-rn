f091bc66adb03ec81995f8fad1c9a59b37355298
lkc <development@watchdict.com>
2018-12-31 00:09:04 +0800

    Update

---
+++ components/ConfirmButton.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import Button, {ButtonProps} from "./Button";
+import ConfirmModal from "./ConfirmModal";
+
+interface ConfirmButtonProps extends ButtonProps {
+    confirmMessage?: string
+    title?: string
+}
+
+const ConfirmButton = (props: ConfirmButtonProps) => {
+    const {confirmMessage, onPress, title} = props;
+    return (
+        <ConfirmModal
+            onConfirmClick={onPress}
+            buttonTrigger={(
+                <Button
+                    {...props}
+                    onPress={() => this.setState({modalVisible: true})}/>
+            )}
+            hint={confirmMessage}
+            title={title}/>
+    )
+};
+
+export default ConfirmButton
