135c451f623b5f42b325400e85c5be3048f4ffe7
lkc <development@watchdict.com>
2019-03-13 12:21:08 +0800

    Update

--- android/app/src/main/java/com/chung_rn/MainApplication.java
+++ android/app/src/main/java/com/chung_rn/MainApplication.java
@@ -3,6 +3,7 @@ package com.chung_rn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new FastImageViewPackage(),
             new RNDeviceInfo(),
             new VectorIconsPackage(),
