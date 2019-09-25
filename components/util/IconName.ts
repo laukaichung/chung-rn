4020070548add4eb74243a59a3f2fa2da5bab14a
lkc <development@watchdict.com>
2019-09-25 16:21:21 +0800

    Update

--- components/util/IconName.ts
+++ components/util/IconName.ts
@@ -1,6 +1,10 @@
 enum IconName {
     ellipsisH = "ellipsis-h",
     pencil = "pencil",
-    caretSquareODown = "caret-square-o-down"
+    caretSquareODown = "caret-square-o-down",
+    star = "star",
+    sliders = "sliders",
+    balanceScale = "balance-scale",
+
 }
 export default IconName;
